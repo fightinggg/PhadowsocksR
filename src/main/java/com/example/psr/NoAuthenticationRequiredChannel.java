cd1130397bd6751827480f55946d2ae9cbe490ba
hellowang <hellowang@tencent.com>
2021-09-01 09:23:17 +0800

    feat: add Network unreachable

--- src/main/java/com/example/psr/NoAuthenticationRequiredChannel.java
+++ src/main/java/com/example/psr/NoAuthenticationRequiredChannel.java
@@ -111,79 +111,87 @@ public class NoAuthenticationRequiredChannel extends ChannelInboundHandlerAdapte
                  *           o  X'04'
                  *
                  */
-                byte[] bytes = ByteBufUtils.readAll(msg);
-                log.debug("{}", new ToStringObject(() -> ByteBufVisiable.toString("client >> ", bytes)));
-                StringBuilder host = new StringBuilder();
-                int port = 80;
-                if (bytes[3] == 1) {
-                    host = new StringBuilder("%d.%d.%d.%d".formatted(bytes[4] & 0xff, bytes[5] & 0xff, bytes[6] & 0xff, bytes[7] & 0xff));
-                    port = ((bytes[bytes.length - 2] & 0xff) << 8) | (bytes[bytes.length - 1] & 0xff);
-                } else if (bytes[3] == 3) {
-                    for (int i = 0; i < bytes[4]; i++) {
-                        host.append((char) (bytes[5 + i]));
+                try {
+                    byte[] bytes = ByteBufUtils.readAll(msg);
+                    log.debug("{}", new ToStringObject(() -> ByteBufVisiable.toString("client >> ", bytes)));
+                    StringBuilder host = new StringBuilder();
+                    int port = 80;
+                    if (bytes[3] == 1) {
+                        host = new StringBuilder("%d.%d.%d.%d".formatted(bytes[4] & 0xff, bytes[5] & 0xff, bytes[6] & 0xff, bytes[7] & 0xff));
+                        port = ((bytes[bytes.length - 2] & 0xff) << 8) | (bytes[bytes.length - 1] & 0xff);
+                    } else if (bytes[3] == 3) {
+                        for (int i = 0; i < bytes[4]; i++) {
+                            host.append((char) (bytes[5 + i]));
+                        }
+                        port = ((bytes[bytes.length - 2] & 0xff) << 8) | (bytes[bytes.length - 1] & 0xff);
+                    } else {
+                        throw new RuntimeException("error");
                     }
-                    port = ((bytes[bytes.length - 2] & 0xff) << 8) | (bytes[bytes.length - 1] & 0xff);
-                } else {
-                    throw new RuntimeException("error");
+                    log.debug("proxy to " + host + ":" + port);
+                    client = new TcpClient(host.toString(), port, new SimpleChannelInboundHandler<>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext proxyCtx, Object proxyMsg) throws Exception {
+                            byte[] bytes = ByteBufUtils.readAll((ByteBuf) proxyMsg);
+                            log.debug("{}", new ToStringObject(() -> ByteBufVisiable.toString("server >> ", bytes)));
+                            ctx.writeAndFlush(Unpooled.copiedBuffer(bytes));
+                        }
+                    });
+                }catch (Exception e){
+                    /*
+                     * The SOCKS request information is sent by the client as soon as it has
+                     *    established a connection to the SOCKS server, and completed the
+                     *    authentication negotiations.  The server evaluates the request, and
+                     *    returns a reply formed as follows:
+                     *
+                     *         +----+-----+-------+------+----------+----------+
+                     *         |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+                     *         +----+-----+-------+------+----------+----------+
+                     *         | 1  |  1  | X'00' |  1   | Variable |    2     |
+                     *         +----+-----+-------+------+----------+----------+
+                     *
+                     *      Where:
+                     *
+                     *           o  VER    protocol version: X'05'
+                     *           o  REP    Reply field:
+                     *              o  X'00' succeeded
+                     *              o  X'01' general SOCKS server failure
+                     *              o  X'02' connection not allowed by ruleset
+                     *              o  X'03' Network unreachable
+                     *              o  X'04' Host unreachable
+                     *              o  X'05' Connection refused
+                     *              o  X'06' TTL expired
+                     *              o  X'07' Command not supported
+                     *              o  X'08' Address type not supported
+                     *              o  X'09' to X'FF' unassigned
+                     *           o  RSV    RESERVED
+                     *           o  ATYP   address type of following address
+                     *              o  IP V4 address: X'01'
+                     *              o  DOMAINNAME: X'03'
+                     *              o  IP V6 address: X'04'
+                     *           o  BND.ADDR       server bound address
+                     *           o  BND.PORT       server bound port in network octet order
+                     *
+                     *    Fields marked RESERVED (RSV) must be set to X'00'.
+                     *
+                     *    If the chosen method includes encapsulation for purposes of
+                     *    authentication, integrity and/or confidentiality, the replies are
+                     *    encapsulated in the method-dependent encapsulation.
+                     *
+                     */
+                    byte[] resBytes = new byte[]{5, 3, 0, 1, (byte) 192, (byte) 168, 1, 1, 0, 80};
+                    log.debug("{}", new ToStringObject(() -> ByteBufVisiable.toString("client << ", resBytes)));
+                    ctx.writeAndFlush(Unpooled.copiedBuffer(resBytes));
+                    ctx.close();
                 }
-                log.debug("proxy to " + host + ":" + port);
-                client = new TcpClient(host.toString(), port, new SimpleChannelInboundHandler<>() {
-                    @Override
-                    protected void channelRead0(ChannelHandlerContext proxyCtx, Object proxyMsg) throws Exception {
-                        byte[] bytes = ByteBufUtils.readAll((ByteBuf) proxyMsg);
-                        log.debug("{}", new ToStringObject(() -> ByteBufVisiable.toString("server >> ", bytes)));
-                        ctx.writeAndFlush(Unpooled.copiedBuffer(bytes));
-                    }
-                });
 
-
-
-                /*
-                 * The SOCKS request information is sent by the client as soon as it has
-                 *    established a connection to the SOCKS server, and completed the
-                 *    authentication negotiations.  The server evaluates the request, and
-                 *    returns a reply formed as follows:
-                 *
-                 *         +----+-----+-------+------+----------+----------+
-                 *         |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
-                 *         +----+-----+-------+------+----------+----------+
-                 *         | 1  |  1  | X'00' |  1   | Variable |    2     |
-                 *         +----+-----+-------+------+----------+----------+
-                 *
-                 *      Where:
-                 *
-                 *           o  VER    protocol version: X'05'
-                 *           o  REP    Reply field:
-                 *              o  X'00' succeeded
-                 *              o  X'01' general SOCKS server failure
-                 *              o  X'02' connection not allowed by ruleset
-                 *              o  X'03' Network unreachable
-                 *              o  X'04' Host unreachable
-                 *              o  X'05' Connection refused
-                 *              o  X'06' TTL expired
-                 *              o  X'07' Command not supported
-                 *              o  X'08' Address type not supported
-                 *              o  X'09' to X'FF' unassigned
-                 *           o  RSV    RESERVED
-                 *           o  ATYP   address type of following address
-                 *              o  IP V4 address: X'01'
-                 *              o  DOMAINNAME: X'03'
-                 *              o  IP V6 address: X'04'
-                 *           o  BND.ADDR       server bound address
-                 *           o  BND.PORT       server bound port in network octet order
-                 *
-                 *    Fields marked RESERVED (RSV) must be set to X'00'.
-                 *
-                 *    If the chosen method includes encapsulation for purposes of
-                 *    authentication, integrity and/or confidentiality, the replies are
-                 *    encapsulated in the method-dependent encapsulation.
-                 *
-                 */
                 byte[] resBytes = new byte[]{5, 0, 0, 1, (byte) 192, (byte) 168, 1, 1, 0, 80};
                 log.debug("{}", new ToStringObject(() -> ByteBufVisiable.toString("client << ", resBytes)));
                 ctx.writeAndFlush(Unpooled.copiedBuffer(resBytes));
 
                 state = "proxy";
+
+
+
             }),
             Map.entry("proxy", (ctx, msg) -> {
                 byte[] bytes = ByteBufUtils.readAll(msg);
